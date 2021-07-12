a856bce039253033aab17d7368c31bb6c906935a
sin314259cos <763493572@qq.com>
2021-07-12 15:33:53 +0800

    file 1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.btn1);
+        final TextView tv1 = findViewById(R.id.tv1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv1.setText("tql");
+                Log.d("search","change");
+            }
+        });
+    }
+}
+
